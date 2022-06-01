4cfcabe9ae1a9ce2c6725eac65bcb72ac3d578e4
Gadia <GadiaEX@users.noreply.github.com>
2022-06-01 10:27:03 +0800

    子弹完成碰撞检测，但是扣血没完成

--- OneCanRunIsOk/Assets/Scripts/Game/Share/WeaponController.cs
+++ OneCanRunIsOk/Assets/Scripts/Game/Share/WeaponController.cs
@@ -6,6 +6,21 @@ namespace OneCanRun.Game.Share
     public class WeaponController : MonoBehaviour
     {
         public GameObject bullet;
+
+        public float cooldownSpeed;
+
+        public float fireRate;
+
+        public float recoilCooldown;
+
+        private float accuracy;
+
+        public float maxSpreadAngle;
+
+        public float timeTillMaxSpread;
+
+
+        public GameObject shootPoint;
         // Start is called before the first frame update
         void Start()
         {
@@ -15,13 +30,30 @@ namespace OneCanRun.Game.Share
         // Update is called once per frame
         void Update(){
 
+            if (Input.GetKeyDown(KeyCode.Space)){
+                trigger();
+            }
 
         }
 
-        private void trigger(){
-            GameObject tmp = new GameObject();
 
 
+        private void trigger()
+        {
+            RaycastHit hit;
+
+            Quaternion fireRotation = Quaternion.LookRotation(transform.forward);
+
+            float currentSpread = Mathf.Lerp(0.0f, maxSpreadAngle, accuracy / timeTillMaxSpread);
+
+            fireRotation = Quaternion.RotateTowards(fireRotation, Random.rotation, Random.Range(0.0f, currentSpread));
+
+            Physics.Raycast(transform.position, fireRotation * Vector3.forward, out hit, Mathf.Infinity);
+
+            {
+                GameObject tempBullet = Instantiate(bullet, shootPoint.transform.position, fireRotation);
+                tempBullet.GetComponent<BulletController>().hitPoint = hit.point;
+            }
         }
     }
 
