ac2b852cb9437bed301cca456f27c664bce54ec1
ExtrAzzzz <842414463@qq.com>
2022-06-06 22:39:03 +0800

    UI添加

--- OneCanRunIsOk/Assets/Scripts/UI/WeaponHudManager.cs
+++ OneCanRunIsOk/Assets/Scripts/UI/WeaponHudManager.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using UnityEngine;
 using OneCanRun.Game;
 using OneCanRun.GamePlay;
+using System.Collections.Generic;
+using OneCanRun.Game.Share;
 
 namespace OneCanRun.UI
 {
-    public class WeaponHudManager : MonoBehaviour
+    public class WeaponHUDManager : MonoBehaviour
     {
         [Tooltip("UI panel containing the layoutGroup for displaying weapon ammo")]
-        public RectTransform AmmoPanel; 
+        public RectTransform AmmoPanel;
 
         [Tooltip("Prefab for displaying weapon ammo")]
         public GameObject AmmoCounterPrefab;
@@ -16,16 +18,58 @@ namespace OneCanRun.UI
         PlayerWeaponsManager m_PlayerWeaponsManager;
         List<AmmoCounter> m_AmmoCounters = new List<AmmoCounter>();
 
-        // Start is called before the first frame update
         void Start()
         {
+            Debug.Log(321);
+            m_PlayerWeaponsManager = FindObjectOfType<PlayerWeaponsManager>();
+            DebugUtility.HandleErrorIfNullFindObject<PlayerWeaponsManager, WeaponHUDManager>(m_PlayerWeaponsManager,
+                this);
 
+            WeaponController activeWeapon = m_PlayerWeaponsManager.GetActiveWeapon();
+            if (activeWeapon)
+            {
+                AddWeapon(activeWeapon, m_PlayerWeaponsManager.ActiveWeaponIndex);
+                ChangeWeapon(activeWeapon);
+            }
+
+            m_PlayerWeaponsManager.OnAddedWeapon += AddWeapon;
+            m_PlayerWeaponsManager.OnRemovedWeapon += RemoveWeapon;
+            m_PlayerWeaponsManager.OnSwitchedToWeapon += ChangeWeapon;
         }
 
-        // Update is called once per frame
-        void Update()
+        void AddWeapon(WeaponController newWeapon, int weaponIndex)
         {
+            GameObject ammoCounterInstance = Instantiate(AmmoCounterPrefab, AmmoPanel);
+            AmmoCounter newAmmoCounter = ammoCounterInstance.GetComponent<AmmoCounter>();
+            DebugUtility.HandleErrorIfNullGetComponent<AmmoCounter, WeaponHUDManager>(newAmmoCounter, this,
+                ammoCounterInstance.gameObject);
 
+            newAmmoCounter.Initialize(newWeapon, weaponIndex);
+
+            m_AmmoCounters.Add(newAmmoCounter);
+        }
+
+        void RemoveWeapon(WeaponController newWeapon, int weaponIndex)
+        {
+            int foundCounterIndex = -1;
+            for (int i = 0; i < m_AmmoCounters.Count; i++)
+            {
+                if (m_AmmoCounters[i].WeaponCounterIndex == weaponIndex)
+                {
+                    foundCounterIndex = i;
+                    Destroy(m_AmmoCounters[i].gameObject);
+                }
+            }
+
+            if (foundCounterIndex >= 0)
+            {
+                m_AmmoCounters.RemoveAt(foundCounterIndex);
+            }
+        }
+
+        void ChangeWeapon(WeaponController weapon)
+        {
+            UnityEngine.UI.LayoutRebuilder.ForceRebuildLayoutImmediate(AmmoPanel);
         }
     }
 }
