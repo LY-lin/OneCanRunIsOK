edc7834fbe6a0c98e5afb2cbf22b522913919926
C-lzwy <59250541+C-lzwy@users.noreply.github.com>
2022-06-12 11:36:44 +0800

    添加交互系统

--- OneCanRunIsOk/Assets/Scripts/GamePlay/Pickups/WeaponPickup.cs
+++ OneCanRunIsOk/Assets/Scripts/GamePlay/Pickups/WeaponPickup.cs
@@ -9,9 +9,15 @@ namespace OneCanRun.GamePlay
         [Tooltip("´ýÊ°È¡ÎäÆ÷")]
         public WeaponController WeaponPrefab;
 
+        Interactive m_Interactive;
+
         protected override void Start()
         {
             base.Start();
+   
+            m_Interactive = GetComponent<Interactive>();
+            DebugUtility.HandleErrorIfNullGetComponent<Interactive, Pickup>(m_Interactive, this, gameObject);
+            
 
             // Set all children layers to default (to prefent seeing weapons through meshes)
             //ÇÐ»»Í¼²ã
@@ -21,6 +27,34 @@ namespace OneCanRun.GamePlay
                     t.gameObject.layer = 0;
             }
         }
+
+        protected override void Update()
+        {
+            base.Update();
+            if (m_Interactive.hasInteracted == true)
+            {
+                OnPicked(m_Interactive.m_PlayerCharacterController);
+                PickupEvent evt = Events.PickupEvent;
+                evt.Pickup = gameObject;
+                EventManager.broadcast(evt);
+            }
+        }
+
+        void OnTriggerEnter(Collider other) { }
+        ////Åö×²´¥·¢½ûÓÃ
+        //void OnTriggerEnter(Collider other)
+        //{
+        //    PlayerCharacterController pickingPlayer = other.GetComponent<PlayerCharacterController>();
+
+        //    if (pickingPlayer != null)
+        //    {
+        //        OnPicked(pickingPlayer);
+
+        //        PickupEvent evt = Events.PickupEvent;
+        //        evt.Pickup = gameObject;
+        //        EventManager.broadcast(evt);
+        //    }
+        //}
 
         //ÖØÐ´Ê°È¡º¯Êý
         protected override void OnPicked(PlayerCharacterController byPlayer)
