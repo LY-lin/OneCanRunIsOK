47b1720589fa3ca933990309dcb37043382f3c94
“Gao Zhilin” <1228664757@qq.com>
2022-06-13 11:58:43 +0800

    11：58

---
+++ OneCanRunIsOk/Assets/Scripts/UI/InGamePackageManager.cs
@@ -0,0 +1,157 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using OneCanRun.GamePlay;
+using OneCanRun.Game;
+namespace OneCanRun.UI
+{
+    public class InGamePackageManager : MonoBehaviour
+    {
+        [Tooltip("Root GameObject of the menu used to toggle its activation")]
+        public GameObject MenuRoot;
+
+        [Tooltip("Master volume when menu is open")]
+        [Range(0.001f, 1f)]
+        public float VolumeWhenMenuOpen = 0.5f;
+
+        [Tooltip("Slider component for look sensitivity")]
+        public Slider LookSensitivitySlider;
+
+        [Tooltip("Toggle component for shadows")]
+        public Toggle ShadowsToggle;
+
+        [Tooltip("Toggle component for invincibility")]
+        public Toggle InvincibilityToggle;
+
+        [Tooltip("Toggle component for framerate display")]
+        public Toggle FramerateToggle;
+
+        /*
+        [Tooltip("GameObject for the controls")]
+        public GameObject ControlImage;
+        */
+        PlayerInputHandler m_PlayerInputsHandler;
+        Health m_PlayerHealth;
+        //显示帧数
+        //FramerateCounter m_FramerateCounter;
+
+        void Start()
+        {
+            m_PlayerInputsHandler = FindObjectOfType<PlayerInputHandler>();
+            DebugUtility.HandleErrorIfNullFindObject<PlayerInputHandler, InGamePackageManager>(m_PlayerInputsHandler,
+                this);
+
+            m_PlayerHealth = m_PlayerInputsHandler.GetComponent<Health>();
+            DebugUtility.HandleErrorIfNullGetComponent<Health, InGamePackageManager>(m_PlayerHealth, this, gameObject);
+
+            //m_FramerateCounter = FindObjectOfType<FramerateCounter>();
+            //DebugUtility.HandleErrorIfNullFindObject<FramerateCounter, InGameMenuManager>(m_FramerateCounter, this);
+
+            MenuRoot.SetActive(false);
+
+            LookSensitivitySlider.value = m_PlayerInputsHandler.LookSensitivity;
+            LookSensitivitySlider.onValueChanged.AddListener(OnMouseSensitivityChanged);
+
+            ShadowsToggle.isOn = QualitySettings.shadows != ShadowQuality.Disable;
+            ShadowsToggle.onValueChanged.AddListener(OnShadowsChanged);
+
+            InvincibilityToggle.isOn = m_PlayerHealth.Invincible;
+            InvincibilityToggle.onValueChanged.AddListener(OnInvincibilityChanged);
+
+            //FramerateToggle.isOn = m_FramerateCounter.UIText.gameObject.activeSelf;
+            //FramerateToggle.onValueChanged.AddListener(OnFramerateCounterChanged);
+        }
+
+        void Update()
+        {
+            // Lock cursor when clicking outside of menu
+            if (!MenuRoot.activeSelf && Input.GetMouseButtonDown(0))
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+
+            if (Input.GetButtonDown(GameConstants.k_ButtonNamePauseMenu)
+                || (MenuRoot.activeSelf && Input.GetButtonDown(GameConstants.k_ButtonNameCancel)))
+            {
+                Debug.Log("Pause");
+                /*if (ControlImage.activeSelf)
+                {
+                    ControlImage.SetActive(false);
+                    return;
+                }*/
+
+                SetPauseMenuActivation(!MenuRoot.activeSelf);
+
+            }
+
+            if (Input.GetAxisRaw(GameConstants.k_AxisNameVertical) != 0)
+            {
+                if (EventSystem.current.currentSelectedGameObject == null)
+                {
+                    EventSystem.current.SetSelectedGameObject(null);
+                    LookSensitivitySlider.Select();
+                }
+            }
+        }
+
+        public void ClosePauseMenu()
+        {
+            SetPauseMenuActivation(false);
+        }
+
+        void SetPauseMenuActivation(bool active)
+        {
+            MenuRoot.SetActive(active);
+
+            if (MenuRoot.activeSelf)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                Time.timeScale = 0f;
+                //AudioUtility.SetMasterVolume(VolumeWhenMenuOpen);
+
+                EventSystem.current.SetSelectedGameObject(null);
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+                Time.timeScale = 1f;
+                //AudioUtility.SetMasterVolume(1);
+            }
+
+        }
+
+        void OnMouseSensitivityChanged(float newValue)
+        {
+            m_PlayerInputsHandler.LookSensitivity = newValue;
+        }
+
+        void OnShadowsChanged(bool newValue)
+        {
+            QualitySettings.shadows = newValue ? ShadowQuality.All : ShadowQuality.Disable;
+        }
+
+        void OnInvincibilityChanged(bool newValue)
+        {
+            m_PlayerHealth.Invincible = newValue;
+        }
+
+        /*void OnFramerateCounterChanged(bool newValue)
+        {
+            m_FramerateCounter.UIText.gameObject.SetActive(newValue);
+        }*/
+
+        /*public void OnShowControlButtonClicked(bool show)
+        {
+            ControlImage.SetActive(show);
+        }*/
+    }
+}
