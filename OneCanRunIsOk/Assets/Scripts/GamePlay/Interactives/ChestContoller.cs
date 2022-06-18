6c03df6c3ce3337ae475962ffc2aab1ae259f672
“Gao Zhilin” <1228664757@qq.com>
2022-06-18 09:19:10 +0800

    添加了宝箱掉落物的弹出效果

--- OneCanRunIsOk/Assets/Scripts/GamePlay/Interactives/ChestContoller.cs
+++ OneCanRunIsOk/Assets/Scripts/GamePlay/Interactives/ChestContoller.cs
@@ -7,14 +7,14 @@ namespace OneCanRun.GamePlay
     public class ChestContoller : MonoBehaviour
     {
         Interactive m_Interactive;
-        Dropable dropable;
+        //Dropable dropable;
 
         // Start is called before the first frame update
         void Start()
         {
             m_Interactive = GetComponent<Interactive>();
             m_Interactive.beInteracted += beOpened;
-            dropable = GetComponent<Dropable>();
+            //dropable = GetComponent<Dropable>();
         }
 
         // Update is called once per frame
