9fd545a51976ca91f7cb45a0d5dcefd10e95f598
ExtrAzzzz <842414463@qq.com>
2022-06-20 17:28:36 +0800

    技能UI补全，背包武器2逻辑补全

--- OneCanRunIsOk/Assets/Scripts/UI/Backpack.cs
+++ OneCanRunIsOk/Assets/Scripts/UI/Backpack.cs
@@ -134,12 +134,22 @@ namespace OneCanRun.UI
         {
             Debug.Log("Open Backpack");
             Weapon1Name.text = playerWeaponsManager.m_WeaponSlots[0].WeaponName;
-            Weapon2Name.text = playerWeaponsManager.m_WeaponSlots[1].WeaponName;
             txtWeapon1.text = playerWeaponsManager.m_WeaponSlots[0].description;
-            txtWeapon2.text = playerWeaponsManager.m_WeaponSlots[1].description;
             Weapon1Img.sprite = playerWeaponsManager.m_WeaponSlots[0].WeaponImg;
-            Weapon2Img.sprite = playerWeaponsManager.m_WeaponSlots[1].WeaponImg;
 
+            if (playerWeaponsManager.m_WeaponSlots[1] != null)
+            {
+                Weapon2Name.text = playerWeaponsManager.m_WeaponSlots[1].WeaponName;
+                txtWeapon2.text = playerWeaponsManager.m_WeaponSlots[1].description;
+                Weapon2Img.sprite = playerWeaponsManager.m_WeaponSlots[1].WeaponImg;
+                Weapon2Img.gameObject.SetActive(true);
+            }
+            else
+            {
+                Weapon2Name.text = "";
+                txtWeapon2.text = "";
+                Weapon2Img.gameObject.SetActive(false);
+            }
             fSkill.sprite = playerSkillsManager.CurrentSkillInstance.SkillIcon;
             FName.text = playerSkillsManager.CurrentSkillInstance.SkillName;
             Ftext.text = playerSkillsManager.CurrentSkillInstance.SkillDescription;
