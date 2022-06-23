eadd03762a45329ce95b2605afa974b0256b2c29
C-lzwy <59250541+C-lzwy@users.noreply.github.com>
2022-06-23 17:06:33 +0800

    添加召唤技能：召唤无人机

--- OneCanRunIsOk/Assets/Scripts/GamePlay/SkillController.cs
+++ OneCanRunIsOk/Assets/Scripts/GamePlay/SkillController.cs
@@ -163,7 +163,13 @@ namespace OneCanRun.GamePlay
         //summon
         void UseSummonSkill()
         {
-            Debug.Log("Summon!");
+            GameObject SummonInstance = Instantiate(UsingVfx, transform.position + transform.up, Quaternion.identity);
+            WeaponController wc = SummonInstance.GetComponentInChildren<WeaponController>();
+            //SummonInstance.Owner = this.Owner;
+            wc.Owner = this.Owner;
+            Destroy(wc.bulletPoolManager.getCollector(), ExistingTime + 3);
+            Destroy(SummonInstance, ExistingTime);
+            //Debug.Log("Summon!");
         }
 
         //Special - get the using point
