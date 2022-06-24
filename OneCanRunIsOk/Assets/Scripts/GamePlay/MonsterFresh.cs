718718f3f87d12917fb512edc2edbca5e9bcd27a
ExtrAzzzz <842414463@qq.com>
2022-06-25 02:01:19 +0800

    Revert "Merge branch 'new-new-new-branch' of https://github.com/LY-lin/OneCanRunIsOK into new-new-new-branch"
    This reverts commit f569e889c23ea94eac7c397a0a86c5428985cb49, reversing
    changes made to c31cddc951224e1cd852f8eaf28bc3c6ac0b9204.

--- OneCanRunIsOk/Assets/Scripts/GamePlay/MonsterFresh.cs
+++ OneCanRunIsOk/Assets/Scripts/GamePlay/MonsterFresh.cs
@@ -47,6 +47,7 @@ namespace OneCanRun.GamePlay
                 float position_y = 0;
                 float position_z = 0;
                 int typeID = 0;
+                int number = 0;
                 foreach(XmlElement x12 in xl1.ChildNodes){
                     if(x12.Name == "Time"){
                         time = int.Parse(x12.InnerText);
@@ -63,9 +64,13 @@ namespace OneCanRun.GamePlay
                     if (x12.Name == "TypeID"){
                         typeID = int.Parse(x12.InnerText);
                     }
+                    if(x12.Name == "Number"){
+                        number = int.Parse(x12.InnerText);
+
+                    }
                 }
 
-                Game.Share.MonsterFreshInfo tmp = new Game.Share.MonsterFreshInfo(time, position_x, position_y, position_z, typeID);
+                Game.Share.MonsterFreshInfo tmp = new Game.Share.MonsterFreshInfo(time, position_x, position_y, position_z, typeID, number);
                 mMonsterList.Add(tmp);
             }
 
@@ -111,7 +116,7 @@ namespace OneCanRun.GamePlay
         void refreshAllFreeMonster(){
             while (monsterPoolManager.activeNumber < monsterPoolManager.getCacheSize()){
                 //monsterPoolManager.getObject(new Vector3(64, 5, 60));
-                GameObject temp = monsterPoolManager.getObject(new Vector3(42, 0.7f, 22));
+                //GameObject temp = monsterPoolManager.getObject(new Vector3(42, 0.7f, 22));
                 
             }
 
@@ -119,7 +124,7 @@ namespace OneCanRun.GamePlay
 
         void refreshOneWave(){
             for(int i = 0;i < waveNumber; i++){
-                GameObject temp = monsterPoolManager.getObject(new Vector3(42, 0.7f, 22));
+                //GameObject temp = monsterPoolManager.getObject(new Vector3(42, 0.7f, 22));
             }
 
         }
