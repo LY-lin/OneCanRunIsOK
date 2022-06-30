a1c013c6d2c4b53528cca222e0813c73458c81d7
“Gao Zhilin” <1228664757@qq.com>
2022-06-30 17:21:32 +0800

    替换了眼球怪的子弹

--- OneCanRunIsOk/Assets/Scripts/GamePlay/BossAwake.cs
+++ OneCanRunIsOk/Assets/Scripts/GamePlay/BossAwake.cs
@@ -1,5 +1,6 @@
 using UnityEngine.Events;
 using UnityEngine;
+using OneCanRun.Game;
 
 namespace OneCanRun.GamePlay
 {
@@ -7,15 +8,20 @@ namespace OneCanRun.GamePlay
     {
         public UnityAction bossAwake;
         ChestContoller Bosschest;
+        CgManager cgManager;
         // Start is called before the first frame update
         void Start()
         {
             Bosschest = GetComponent<ChestContoller>();
             Bosschest.ChestOpen += ChestWasOpen;
+
+            cgManager = GameObject.FindObjectOfType<CgManager>();
+            DebugUtility.HandleErrorIfNullFindObject<CgManager, BossAwake>(cgManager, this);
         }
         void ChestWasOpen()
         {
             bossAwake?.Invoke();
+            cgManager.PlayCG();
             AudioSource BGM = GameObject.Find("GameManager").GetComponent<AudioSource>();
             BGM.Stop();
             BGM.clip = Resources.Load<AudioClip>("Glenn Stafford - Heaven's Devils");
